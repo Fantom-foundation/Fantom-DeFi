c0f56143a145faf6f2e8b4249e3ebb40ba5cfdfb
Jiri Malek <jirka.malek@gmail.com>
2020-07-16 09:57:59 +0200

    Lock configuration values to be immutable.

--- contracts/liquiditypool/LiquidityPoolConfig.sol
+++ contracts/liquiditypool/LiquidityPoolConfig.sol
@@ -22,12 +22,19 @@ contract LiquidityPoolConfig is Ownable {
     // The value is kept in 4 decimals, e.g. value 15000 => ratio 1.5x
     uint256 public colLiquidationRatio4dec = 15000;
 
+    // colWarningRatio4dec represents a ratio between collateral
+    // value and debt value below which a user should be warned about
+    // the dangerous ratio zone. If the real ratio drops below this value
+    // the liquidation condition will be closely observed by a watchdog.
+    // The value is kept in 4 decimals, e.g. value 22500 => ratio 2.25x
+    uint256 public colWarningRatio4dec = 22500;
+
     // colLowestRatio4dec represents the lowest ratio between collateral
     // value and debt value user can use to borrow tokens against the
     // collateral. Below this ratio a borrow request will be rejected
     // and user needs to increase their collateral to close the deal.
-    // The value is kept in 4 decimals, e.g. value 25000 => ratio 2.5x
-    uint256 public colLowestRatio4dec = 25000;
+    // The value is kept in 4 decimals, e.g. value 25000 => ratio 3x
+    uint256 public colLowestRatio4dec = 30000;
 
     // ratioDecimalsCorrection represents the value to be used to
     // adjust result decimals after applying ratio to a calculation.
@@ -37,26 +44,12 @@ contract LiquidityPoolConfig is Ownable {
     address public nativeToken = 0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF;
 
     // fUsdToken represents the address of the fUSD stable coin token ERC20 contract.
-    address public fUsdToken = 0xC17518AE5dAD82B8fc8b56Fe0295881c30848829;
+    address public fUsdToken = 0xf15ff135dc437a2fd260476f31b3547b84f5dd0b;
 
     // priceOracle represents the address of the price oracle contract used
     // to obtain the current exchange rate between a specified token
     // and the referential fUSD.
-    address public priceOracle = 0xC17518AE5dAD82B8fc8b56Fe0295881c30848829;
-
-    // TradeFeeChanged is emitted on the trade fee change.
-    event TradeFeeChanged(uint256 fee, uint256 timestamp);
-
-    // LoanEntryFeeChanged is emitted on the loan fee change.
-    event LoanEntryFeeChanged(uint256 fee, uint256 timestamp);
-
-    // CollateralLiquidationRatioChanged is emitted on collateral to debt
-    // liquidation ratio changes.
-    event CollateralLiquidationRatioChanged(uint256 rate, uint256 timestamp);
-
-    // CollateralLowestRatioChanged is emitted on the collateral to debt
-    // lowest accepted ratio changes.
-    event CollateralLowestRatioChanged(uint256 ratio, uint256 timestamp);
+    address public priceOracle = 0x122aacb5f8e4a7787131a346a52f0aa78388a676;
 
     // PriceOracleChanged is emitted on the change of the price oracle contract address.
     event PriceOracleChanged(address oracle, uint256 timestamp);
@@ -65,42 +58,10 @@ contract LiquidityPoolConfig is Ownable {
     // management functions below this line
     // ----------------------------------------
 
-    // setTradeFee changes the current trade fee value.
-    // Please make sure to use 4 decimals value to represent the fee,
-    // e.g. value 25 is applied as 0.0025 => 0.25%
-    function setTradeFee(uint256 fee) onlyOwner external {
-        tradeFee4dec = fee;
-        emit TradeFeeChanged(fee, now);
-    }
-
-    // setLoanEntryFee changes the current loan entry fee value.
-    // Please make sure to use 4 decimals value to represent the fee,
-    // e.g. value 25 is applied as 0.0025 => 0.25%
-    function setLoanEntryFee(uint256 fee) onlyOwner external {
-        loanEntryFee4dec = fee;
-        emit LoanEntryFeeChanged(fee, now);
-    }
-
-    // setCollateralLiquidationRatio changes the current ratio
-    // between the collateral and debt below which the debt position
-    // should be liquidated.
-    function setCollateralLiquidationRatio(uint256 ratio) onlyOwner external {
-        colLiquidationRatio4dec = ratio;
-        emit CollateralLiquidationRatioChanged(ratio, now);
-    }
-
-    // setCollateralLowestRatio changes the current ratio
-    // between the collateral and debt below which a borrow request against
-    // the collateral will be rejected.
-    function setCollateralLowestRatio(uint256 ratio) onlyOwner external {
-        colLowestRatio4dec = ratio;
-        emit CollateralLowestRatioChanged(ratio, now);
-    }
-
-    // setCollateralLowestRatio changes the current ratio
-    // between the collateral and debt below which a borrow request against
-    // the collateral will be rejected.
-    function setPriceOracle(address oracle) onlyOwner external {
+    // setPriceOracleReferenceAggregate changes the link to the current version
+    // of the Price Oracle Reference Aggregate. The oracle ref. aggregate is responsible
+    // for providing current oracle price for a given token against the fUSD stable coin.
+    function setPriceOracleReferenceAggregate(address oracle) onlyOwner external {
         priceOracle = oracle;
         emit PriceOracleChanged(oracle, now);
     }
